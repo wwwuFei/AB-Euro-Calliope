071ff9f578d3739c3a045ad645943973c70360ce
Fei Wu <wwwuFei@hotmail.com>
2022-02-10 15:50:30 +0100

    fix test_submit_runs.sh.array.sh

--- test_submit_runs.sh
+++ test_submit_runs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #BSUB -J calliope[1-9]
-#BSUB -n 0
-#BSUB -R "rusage[mem=230G]"
+#BSUB -n 8
+#BSUB -R "rusage[mem=240G]"
 #BSUB -W 24:00
 #BSUB -r
 #BSUB -o log_%I.log
 
-./submit_runs.sh.array.sh ${LSB_JOBINDEX}
+./test_submit_runs.sh.array.sh ${LSB_JOBINDEX}
--- test_submit_runs.sh.array.sh
+++ test_submit_runs.sh.array.sh
@@ -3,15 +3,23 @@
 function process_case () {
     case "$1" in
 
-    1) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_1h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals
-    2) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals
-    3) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_1h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,gas_storage
-    4) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,gas_storage
-    5) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_1h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,biofuel_supply_only_1
-    6) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,biofuel_supply_only_1
-    7) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_1h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,biofuel_supply_only_1,gas_storage
-    8) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,biofuel_supply_only_1,gas_storage
-    9) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_12h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,biofuel_supply_only_1,gas_storage
+    1) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_1h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+    
+    2) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+    
+    3) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_1h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,gas_storage ;;
+    
+    4) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,gas_storage ;;
+    
+    5) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_1h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,biofuel_supply_only_1 ;;
+    
+    6) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,biofuel_supply_only_1 ;;
+    
+    7) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_1h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,biofuel_supply_only_1,gas_storage ;;
+    
+    8) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,biofuel_supply_only_1,gas_storage ;;
+    
+    9) python run /cluster/work/apatt/feiwu/Ancillary-Bioenergy-Paper/fei_run.py res_12h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals,biofuel_supply_only_1,gas_storage ;;  
     
     esac
 }
