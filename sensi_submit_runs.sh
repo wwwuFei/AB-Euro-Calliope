dba0f12ef21293201a6f835dfb45d49351cd8fb0
Fei Wu <wwwuFei@hotmail.com>
2022-02-15 18:41:51 +0100

    add overrides changing opt methods

--- sensi_submit_runs.sh
+++ sensi_submit_runs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#BSUB -J calliope[1-9]
-#BSUB -n 8
+#BSUB -J calliope[1-10]
+#BSUB -n 6
 #BSUB -R "rusage[mem=100G]"
 #BSUB -W 24:00
 #BSUB -r
-#BSUB -o log_%I.log
+#BSUB -o log_sensi_%I.log
 
 ./sensi_submit_runs.sh.array.sh ${LSB_JOBINDEX}
