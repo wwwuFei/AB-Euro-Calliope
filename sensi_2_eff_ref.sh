a037edd5b22898ed95bf42f2c174bd7dc469e823
Fei Wu <wwwuFei@hotmail.com>
2022-02-22 22:18:02 +0100

    new overrides over 5% for energy_eff

--- sensi_2_eff_ref.sh
+++ sensi_2_eff_ref.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#BSUB -J calliope[1-12]
+#BSUB -J calliope[1-18]
 #BSUB -n 6
 #BSUB -R "rusage[mem=60G]"
 #BSUB -W 24:00
 #BSUB -r
-#BSUB -o log_2_sensi_eff_ref_%I.log
+#BSUB -o log_3_sensi_eff_ref_%I.log
 
 ./sensi_2_eff_ref.sh.array.sh ${LSB_JOBINDEX}
--- sensi_2_eff_ref.sh.array.sh
+++ sensi_2_eff_ref.sh.array.sh
@@ -3,25 +3,30 @@
 function process_case () {
     case "$1" in
 
-    1) python fei_sensi_run.py bio_trans_eff_70_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    2) python fei_sensi_run.py bio_trans_eff_80_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    3) python fei_sensi_run.py bio_trans_eff_90_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    4) python fei_sensi_run.py bio_trans_eff_110_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    5) python fei_sensi_run.py bio_trans_eff_120_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    6) python fei_sensi_run.py bio_trans_eff_130_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    7) python fei_sensi_run.py hydrogen_trans_eff_70_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    8) python fei_sensi_run.py hydrogen_trans_eff_80_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    9) python fei_sensi_run.py hydrogen_trans_eff_90_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    10) python fei_sensi_run.py hydrogen_trans_eff_110_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    11) python fei_sensi_run.py hydrogen_trans_eff_120_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-    12) python fei_sensi_run.py hydrogen_trans_eff_130_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
-  
+1) python fei_sensi_run.py bio_trans_eff_95_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+2) python fei_sensi_run.py bio_trans_eff_105_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+3) python fei_sensi_run.py bio_trans_eff_90_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+4) python fei_sensi_run.py bio_trans_eff_110_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+5) python fei_sensi_run.py bio_trans_eff_85_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+6) python fei_sensi_run.py bio_trans_eff_115_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+7) python fei_sensi_run.py hydrogen_trans_eff_95_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+8) python fei_sensi_run.py hydrogen_trans_eff_105_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+9) python fei_sensi_run.py hydrogen_trans_eff_90_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+10) python fei_sensi_run.py hydrogen_trans_eff_110_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+11) python fei_sensi_run.py hydrogen_trans_eff_85_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+12) python fei_sensi_run.py hydrogen_trans_eff_115_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+13) python fei_sensi_run.py electrolysis_eff_95_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+14) python fei_sensi_run.py electrolysis_eff_105_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+15) python fei_sensi_run.py electrolysis_eff_90_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+16) python fei_sensi_run.py electrolysis_eff_110_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+17) python fei_sensi_run.py electrolysis_eff_85_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
+18) python fei_sensi_run.py electrolysis_eff_115_perc,res_2h,industry_fuel,transport,heat,config_overrides,link_cap_dynamic,freeze-hydro-capacities,bio_potential_org0_msw_equals ;;
     esac
 }
 
 if [[ $# -eq 0 ]] ; then
     echo No parameter given, running all runs sequentially...
-    for i in $(seq 1 12); do process_case $i; done
+    for i in $(seq 1 18); do process_case $i; done
 else
     echo Running run $1
     process_case $1
