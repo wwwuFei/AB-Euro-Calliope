2048da591fd96ef55407162b3f0e9c18e4d9892e
Fei Wu <wwwuFei@hotmail.com>
2022-04-08 09:54:59 +0200

    fix .sh

--- sensi_weather_year.sh.array.sh
+++ sensi_weather_year.sh.array.sh
@@ -3,32 +3,22 @@
 function process_case () {
     case "$1" in
 
-    1) python fei_sensi_weather.py 2000 ;;
-    2) python fei_sensi_weather.py 2001 ;;
-    3) python fei_sensi_weather.py 2002 ;;
-    4) python fei_sensi_weather.py 2003 ;;
-    5) python fei_sensi_weather.py 2004 ;;
-    6) python fei_sensi_weather.py 2005 ;;
-    7) python fei_sensi_weather.py 2006 ;;
-    8) python fei_sensi_weather.py 2007 ;;
-    9) python fei_sensi_weather.py 2008 ;;
-    10) python fei_sensi_weather.py 2009 ;;
-    11) python fei_sensi_weather.py 2010 ;;
-    12) python fei_sensi_weather.py 2011 ;;    
-    13) python fei_sensi_weather.py 2012 ;;
-    14) python fei_sensi_weather.py 2013 ;;
-    15) python fei_sensi_weather.py 2014 ;;
-    16) python fei_sensi_weather.py 2015 ;;
-    17) python fei_sensi_weather.py 2016 ;;
-    18) python fei_sensi_weather.py 2017 ;;
-    19) python fei_sensi_weather.py 2018 ;;
+    1) python fei_sensi_weather.py 2010 ;;
+    2) python fei_sensi_weather.py 2011 ;;    
+    3) python fei_sensi_weather.py 2012 ;;
+    4) python fei_sensi_weather.py 2013 ;;
+    5) python fei_sensi_weather.py 2014 ;;
+    6) python fei_sensi_weather.py 2015 ;;
+    7) python fei_sensi_weather.py 2016 ;;
+    8) python fei_sensi_weather.py 2017 ;;
+    9) python fei_sensi_weather.py 2018 ;;
 
     esac
 }
 
 if [[ $# -eq 0 ]] ; then
     echo No parameter given, running all runs sequentially...
-    for i in $(seq 1 19); do process_case $i; done
+    for i in $(seq 1 9); do process_case $i; done
 else
     echo Running run $1
     process_case $1
